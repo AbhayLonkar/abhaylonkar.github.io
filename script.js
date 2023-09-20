5845d41f09ee1e5f4855ff943ceb72306af31ede
Abhay Lonkar <136677041+AbhayLonkar@users.noreply.github.com>
2023-09-20 11:45:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function menutoggle(){
+    const icon = document.querySelector('.toggle');
+    const menu = document.querySelector('.navlinks');
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
