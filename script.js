edca61022b12d4b6b598bd52c3f4039d396b2bc7
Abhay Lonkar <abhaylonkar9@gmail.com>
2025-04-25 12:19:55 +0530

    Replace all files with a under development template

--- script.js
+++
@@ -1,6 +0,0 @@
-function menutoggle(){
-    const icon = document.querySelector('.toggle');
-    const menu = document.querySelector('.navlinks');
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
